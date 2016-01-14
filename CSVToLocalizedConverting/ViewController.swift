7938ec541392e43e819c7f92efc3f5d3c537cdcf
Zmicier Biesau <zmicier.biesau@gmail.com>
2016-01-14 10:19:37 +0300

    Initial Commit

---
+++ CSVToLocalizedConverting/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CSVToLocalizedConverting
+//
+//  Created by Zmicier Biesau on 14.1.16.
+//  Copyright © 2016 Maxvale Ind. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
